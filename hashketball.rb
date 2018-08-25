f898938088457e1367501db49460b52648f6e18d
Sam Ashtar <samashtarm@gmail.com>
2018-08-25 03:10:37 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -205,7 +205,6 @@ end
 def player_numbers(team)
   array2 = []
   game_hash.each do |location, team_data|
-     binding.pry
     if team_data[:team_name] = team
       team_data.each do |attribute, values|
           if attribute == :players && 
